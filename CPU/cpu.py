c41f3f1e118094b0e42c5fefe00dd02ebdecff4e
alex-shapovalov <shapovalov.alex@pm.me>
2024-01-19 23:39:26 -0500

    initial class work 1-19-2024

--- CPU/cpu.py
+++ CPU/cpu.py
@@ -2,6 +2,18 @@
 #CS 5220
 #Programming Assignment #1, Instruction Processing
 
+NOOP = 0
+ADD = 1
+ADDI = 2
+BEQ = 3
+JAL = 4
+LW = 5
+SW = 6
+RETURN = 7
+SUB = 8
+SUBI = 9
+JALR = 10
+
 class CPU:
     def __init__(self):
         pc = 0
@@ -22,3 +34,6 @@ def build_instruction(opcode, Rd, Rs1, Rs2, immed):
     insrt = opcode << 28
     return_int = 0
     return return_int
+
+def main():
+    i0 = build_instruction(NOOP, None, None, None, None)
