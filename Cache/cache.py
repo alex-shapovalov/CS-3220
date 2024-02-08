532e09850fbc4c4b88d727906cc7c4b95d3d4b7c
alex-shapovalov <shapovalov.alex@pm.me>
2024-02-08 14:59:27 -0500

    initial setup 2-8-2024

--- Cache/cache.py
+++ Cache/cache.py
@@ -1,3 +1,24 @@
 #Alex Shapovalov
 #CS 5220
 #Programming Assignment #2, Cache
+
+class Cache:
+    def __init__(self):
+        self.variable
+
+class Set:
+    def __init__(self):
+        self.variable
+
+class Block:
+    def __init__(self):
+        self.variable
+
+def readWord(address):
+    a = 1
+def writeWord(address, word):
+    a = 1
+
+def main():
+    a = 1
+main()
