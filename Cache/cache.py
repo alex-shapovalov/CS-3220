f893523f40f68cb3c1360f9cd4531c7a79408027
alex-shapovalov <shapovalov.alex@pm.me>
2024-02-11 18:23:07 -0500

    work day 2-11-2024

--- Cache/cache.py
+++ Cache/cache.py
@@ -2,17 +2,33 @@
 #CS 5220
 #Programming Assignment #2, Cache
 
-class Cache:
-    def __init__(self):
-        self.variable
-
-class Set:
-    def __init__(self):
-        self.variable
-
 class Block:
-    def __init__(self):
-        self.variable
+    def __init__(self, size):
+        self.block = [0] * size
+        #TODO:
+        self.tag = 0
+        self.clean = True
+        self.status = False
+
+class Cache:
+    def __init__(self, address, cache, block, associativity, write):
+        self.address = address #address size
+        self.cache = cache #cache size
+        self.block = block #block size
+        self.associativity = associativity #number of blocks per set
+        self.write = write #write type
+
+        self.block = [Block(block)] * (cache // block) #create an array of blocks with size block
+
+        numSets = (cache // associativity) // block #figure out how many sets we need
+        self.set = {} #sets defined as a dictionary
+        z = 0
+        for x in range(1, numSets + 1): #creates sets based on block size and associativity, self.sets is a dictionary of 1 or more blocks
+            self.set["set " + str(x)] = []
+            for y in range(min(numSets, associativity)):
+                if z < len(self.block):
+                    self.set["set " + str(x)].append(self.block[z])
+                    z += 1
 
 def readWord(address):
     a = 1
@@ -20,5 +36,10 @@ def writeWord(address, word):
     a = 1
 
 def main():
-    a = 1
+    addressLength = 16
+    cache = Cache(addressLength, 256, 16, 1, "Null")
+    memory = [0] * (2 ** addressLength)
+
+    print(len(memory), len(cache.block), cache.set)
+
 main()
