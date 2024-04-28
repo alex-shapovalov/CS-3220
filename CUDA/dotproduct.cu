cddb76a1b28743c4f8ac30cf6f129967f183a83e
alex-shapovalov <shapovalov.alex@pm.me>
2024-04-27 20:01:46 -0400

    initial setup 4-27-2024

---
+++ CUDA/dotproduct.cu
@@ -0,0 +1,16 @@
+//Alex Shapovalov
+//CS 3220
+//Programming Assignment #5, CUDA
+
+#include <stdio.h>
+#include <stdlib.h>
+
+__global__
+void dotp( float *u, float *v, float *partialSums, int n ) {
+
+}
+
+int main() {
+
+  return 0;
+}
---
+++ CUDA/matrixvector.cu
@@ -0,0 +1,16 @@
+//Alex Shapovalov
+//CS 3220
+//Programming Assignment #5, CUDA
+
+#include <stdio.h>
+#include <stdlib.h>
+
+__global__
+void MxV( float *M, float *x, float *y, int n) {
+
+}
+
+int main() {
+
+  return 0;
+}
